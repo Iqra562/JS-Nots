41bea66f33c50b465ab0e5402d4cdf0e8b92165d
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2024-10-04 10:52:08 -0700

    Update currying.js

--- currying.js
+++ currying.js
@@ -1,1 +1,1 @@
-
+Currying in JavaScript is a technique in functional programming where a function that takes multiple arguments is transformed into a sequence of functions that each take a single argument.
